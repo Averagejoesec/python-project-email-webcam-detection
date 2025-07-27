d619dc76bf817ddf66568048283d8570c234d306
averagejoesec <gonzalezjlusmc@gmail.com>
2025-07-27 17:28:57 +0200

    restart project

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
 import cv2
-import time
 
 video = cv2.VideoCapture(0)
-time.sleep(1)
+check, frame = video.read()
 
-while True:
-
-    check, frame = video.read()
-    cv2.imshow("My video", frame)
-
-    key = cv2.waitKey(1)
-
-    if key == ord("q"):
-        break
-
-video.release()
-
+print(check)
+print(frame)
