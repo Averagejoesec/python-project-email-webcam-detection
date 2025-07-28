f339b20b7accbaaedfebd0abe1c077b499b727aa
averagejoesec <gonzalezjlusmc@gmail.com>
2025-07-28 14:04:17 +0200

    completed project

--- emailing.py
+++ emailing.py
@@ -1,2 +1,26 @@
-def send_email():
-    print('Email sent!')
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+PASSWORD = "your_password_from_secure_location"
+SENDER = "your_email_username@gmail.com"
+RECEIVER = "your_email_username@gmail.com"
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "New customer showed up!"
+    email_message.set_content("Hey, we just saw a new customer!")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
+if __name__ == "__main__":
+    send_email()
